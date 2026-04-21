8b73de085993586328ac9299eff693f45c640b9b
wassimabdallah <wassim.abdallah@pixiimotors.com>
2026-04-21 17:26:09 +0100

    feat: add the voltage and current data byte execution.

--- src/main.cpp
+++ src/main.cpp
@@ -15,8 +15,14 @@ void vCanRxTask() {
             // On filtre : on ne s'intéresse qu'à la trame ID 0x100 (ex: Trame Moteur)
             if (frame.can_id == 0x100) {
                 int speed = frame.data[0]; // On suppose que la donnée 0 est la vitesse
+                int voltage = frame.data[1] ;
+                int current = frame.data[2] ;
+
                 std::cout << "[CAN RX] Real Frame ID 0x100 received! Extracted speed: " << speed << " km/h" << std::endl;
+                std::cout << "[CAN RX] Voltage: " << voltage << " V, Current: " << current << " A" << std::endl;
                 vehicleDataQueue.push(speed);
+                vehicleDataQueue.push(voltage);
+                vehicleDataQueue.push(current) ;
             }
         }
     }
@@ -24,10 +30,22 @@ void vCanRxTask() {
 
 void vProcessingTask() {
     int receivedSpeed = 0;
+    int receivedVoltage = 0 ;
+    int receivedCurrent = 0 ;
     while (true) {
         if (vehicleDataQueue.pop(receivedSpeed)) {
             std::cout << "[PROCESSING] Analyzing speed: " << receivedSpeed << " km/h - Status: ";
             if (receivedSpeed > 120) std::cout << "CRITICAL (Overspeed!)" << std::endl;
+            else std::cout << "OK" << std::endl;
+        }
+        if (vehicleDataQueue.pop(receivedVoltage)) {
+            std::cout << "[PROCESSING] Analyzing voltage: " << receivedVoltage << " V - Status: ";
+            if (receivedVoltage < 11) std::cout << "WARNING (Low Voltage!)" << std::endl;
+            else std::cout << "OK" << std::endl;
+        }
+        if (vehicleDataQueue.pop(receivedCurrent)) {
+            std::cout << "[PROCESSING] Analyzing current: " << receivedCurrent << " A - Status: ";
+            if (receivedCurrent > 50) std::cout << "WARNING (High Current!)" << std::endl;
             else std::cout << "OK" << std::endl;
         }
     }
